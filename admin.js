f9b345ba1112f23756059e1bba550139e367e6e5
iqbalkhan11296-coder <iqbalkhan11296@gmail.com>
2026-07-24 20:26:12 +0530

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,27 @@
+async function loadData(){
+
+const {data,error}= await supabaseClient
+.from("contacts")
+.select("*")
+.order("id",{ascending:false});
+
+
+let table="";
+
+data.forEach(item=>{
+
+table += `
+<tr>
+<td>${item.name}</td>
+<td>${item.place}</td>
+<td>${item.mobile}</td>
+</tr>
+`;
+
+});
+
+document.getElementById("data").innerHTML=table;
+
+}
+
+loadData();
